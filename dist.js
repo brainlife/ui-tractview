09cd902fb319fc3ac177d1768bdaab98b2fb8132
stevengeeky <stevengeeky@gmail.com>
2018-05-07 18:46:31 +0000

    move bundled libraries to dist.bundled.js and migrate main.js code to index.html

---
+++ dist.js
@@ -0,0 +1,5 @@
+window.ndarray = require('ndarray');
+window.nifti = require('nifti-js');
+window.async = require('async');
+window.Plotly = require('plotly.js/lib/core');
+window.Plotly.register([require('plotly.js/lib/histogram')]);
