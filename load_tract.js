ace2f2b141f04ef6b17f7bad87e2fc7badee9a61
Soichi Hayashi <hayashis@iu.edu>
2020-02-24 18:27:18 -0500

    updated the way bundle unwinding is detected

--- load_tract.js
+++ load_tract.js
@@ -12,13 +12,22 @@ onmessage = function(e) {
         var bundle = json.coords;
         console.log(tract.url, bundle.length);
 
-        if(bundle[0] && bundle[0].length > 3) bundle = bundle[0]; //unwind 
+        //if(bundle[0] && bundle[0].length > 3) bundle = bundle[0]; //unwind 
+        let depth = 0;
+        let node = bundle[0];
+        while(Array.isArray(node)) {
+            depth++;
+            node = node[0];
+        }
+        if(depth > 3) {
+            console.log("unwinding",depth);
+            bundle = bundle[0];
+        }
 
         //convert each bundle to threads_pos array
         var threads_pos = [];
         bundle.forEach(function(fascicle) {
             if (fascicle.length == 1) fascicle = fascicle[0]; 
-
             var xs = fascicle[0];
             var ys = fascicle[1];
             var zs = fascicle[2];
